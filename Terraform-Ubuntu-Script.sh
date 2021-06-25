c788e090196d022d5f88956b4a70a39b8b5b0375
melchilio <65927599+melchilio@users.noreply.github.com>
2021-06-25 10:32:10 -0600

    Update Terraform-Ubuntu-Script.sh

--- Terraform-Ubuntu-Script.sh
+++ Terraform-Ubuntu-Script.sh
@@ -6,6 +6,6 @@ sudo apt-get install wget unzip -y
 
 sudo wget https://releases.hashicorp.com/terraform/1.0.1/terraform_1.0.1_linux_amd64.zip
 
-sudo unzip ./terraform_0.12.18_linux_amd64.zip –d /usr/local/bin
+sudo unzip ./terraform_1.0.1_linux_amd64.zip –d /usr/local/bin
 
 terraform -v
