4b97670695f731fa19edcc296dff8ca9e8ab1406
melchilio <65927599+melchilio@users.noreply.github.com>
2021-06-25 09:14:34 -0600

    Update Terraform-Centos7-Script.sh

--- Terraform-Centos7-Script.sh
+++ Terraform-Centos7-Script.sh
@@ -6,7 +6,7 @@ sudo yum install wget unzip -y
 
 sudo wget https://releases.hashicorp.com/terraform/1.0.1/terraform_1.0.1_linux_amd64.zip
 
-sudo unzip terraform_0.12.18_linux_amd64.zip
+sudo unzip terraform_1.0.1_linux_amd64.zip
 
 sudo mv terraform /usr/local/bin/
 
